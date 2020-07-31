3bd61ea2a430893de30313ddf924609146b432da
Yen <qqgnoe466263@gmail.com>
2020-07-31 19:01:36 +0800

    update testing tools

--- scripts/driver.py
+++ scripts/driver.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import os
 import subprocess
@@ -6,27 +6,26 @@ from subprocess import PIPE, Popen
 
 
 test_case = {
-    1: "test_pt.cmd",        
-    2: "test_pt1.cmd",        
-    3: "test_pt2.cmd",        
+    0: "test_pt.cmd",        
+    1: "test_pt5.cmd",        
+    2: "test_pt2.cmd",        
+    3: "test_pt1.cmd",        
     4: "test_pt3.cmd",        
     5: "test_pt4.cmd",        
-    6: "test_pt5.cmd",        
-    7: "test_pt6.cmd",        
+    6: "test_pt6.cmd",        
 }
 
-testList = test_case.keys()
 
-for i in range(len(testList)):
-    f = open("traces/" + test_case[testList[i]], "r")
+for i in range(len(test_case)):
+    f = open("traces/" + test_case[i], "r")
     testing_cmd = f.readline()
     content = f.readline()[:-1]
 
     s = (testing_cmd.split('\n')[0]).split(' ')
     proc = subprocess.Popen(s, stdout=subprocess.PIPE) 
     result = proc.stdout.readline()
-    print '[+] Test   :',testing_cmd[:-1]
-    if result.find(content) > -1:
-        print '[*] result : pass'
+    print('[+] Test   : {}'.format(testing_cmd[:-1]))
+    if str(result).find(content) > -1:
+        print('[*] result : Read Access\n')
     else:
-        print '[*] result : fail'
+        print('[*] result : Read Fail\n')
